eda3806b07d52c8726e216e62d351433521ffd2a
AaronGeoLim <aarongeo225@gmail.com>
2024-11-04 21:51:37 +0800

    lab9

---
+++ Assignment_3_LoopingStatements/WhileLoop.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+
+while name == "":
+    print("Enter your name please")
+    name = input ("Enter your name: ")
+print(f"That's a cool name, nice to meet you {name}!")
